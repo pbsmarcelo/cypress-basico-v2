be1410541aae2b79e4b7cafffcd6f8456e2e6f90
Marcelo <pbsmarcelo@gmail.com>
2023-01-09 23:53:00 -0300

    Cap 7 - Exercicio extra 2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -143,7 +143,7 @@ it('seleciona um arquivo da pasta fixtures', function(){
         })
 })
 
-it.only('seleciona um arquivo simulando um drag-and-drop', function(){
+it('seleciona um arquivo simulando um drag-and-drop', function(){
     cy.get('input[type="file"]#file-upload')
         .should('not.have.value')
         .selectFile('./Cypress/fixtures/example.json', { action: "drag-drop"})
@@ -151,6 +151,16 @@ it.only('seleciona um arquivo simulando um drag-and-drop', function(){
             console.log($input)
             expect($input[0].files[0].name).to.equal('example.json')
         })
+})
+
+it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function(){
+    cy.fixture('example.json').as('SampleFile')
+    cy.get('input[type="file"]#file-upload')
+    .selectFile('@SampleFile')
+    .should(function($input){
+        console.log($input)
+        expect($input[0].files[0].name).to.equal('example.json')
+    })
 })
   })
   
