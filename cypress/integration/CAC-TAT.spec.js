008e96ba1995c6e256d5045e47fcf4899710c9be
Marcelo <pbsmarcelo@gmail.com>
2023-01-12 23:15:29 -0300

    Cap 12 cy.request() fazendo uma requisição HTTP

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -223,12 +223,24 @@ it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
       .should('not.be.visible')
   })
 
-  it.only('preenche a area de texto usando o comando invoke', function () {
+  it('preenche a area de texto usando o comando invoke', function () {
     const longText = Cypress._.repeat('0123456789', 20)
 
     cy.get('#open-text-area')
         .invoke('val', longText)
         .should('have.value', longText)
+  })
+
+  it.only('faz uma requisição HTTP', function() {
+    cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+        .should(function (response) {
+            //console.log(response)
+            //desestruturar no javascript
+            const {status, statusText, body} = response
+            expect(status).to.equal(200)
+            expect(statusText).to.equal('OK')
+            expect(body).to.include('CAC TAT')
+        })
   })
 
   })
